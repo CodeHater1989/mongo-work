0e0be8322ff7a51abeb669452c57e71a5db7c5bd
wanglei <wanglei02@cis.com.cn>
2016-08-18 15:42:09 +0800

    isLast API

--- src/main/java/oracle2Mongo/OracleStream.java
+++ src/main/java/oracle2Mongo/OracleStream.java
@@ -21,70 +21,48 @@ import java.util.Map;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OracleStream {
-    private String          url;
-    private String          sql;
-    private RowProcess      process;
+    private String url;
+    private String sql;
+    private RowProcess process;
     private ProcessCallback callback;
 
     public void processDataStream() {
         int FETCH_SIZE = 10_0000;
 
-        Connection        connection = null;
-        PreparedStatement ps         = null;
-        ResultSet         rs         = null;
+        Connection connection = null;
+        Statement ps = null;
+        ResultSet rs = null;
         try {
             OracleDataSource dataSource = new OracleDataSource();
             dataSource.setURL(url);
 
             connection = dataSource.getConnection();
-            ps         = connection.prepareStatement(sql, ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+            ps = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+//            ps         = connection.prepareStatement(sql, ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
             ps.setFetchSize(FETCH_SIZE);
 
             rs = ps.executeQuery(sql);
 
-            ResultSetMetaData mdrs         = rs.getMetaData();
-            List<String>      columnLabels = new ArrayList<>();
+            ResultSetMetaData mdrs = rs.getMetaData();
+            List<String> columnLabels = new ArrayList<>();
 
             for (int i = 1; i <= mdrs.getColumnCount(); i++) {
                 columnLabels.add(mdrs.getColumnLabel(i));
             }
 
 
-            while (true) {
-                Map row0 = new HashMap();
-                Map row1 = new HashMap();
-                if(rs.next()) {
-                    for (int j = 0; j < columnLabels.size(); j++) {
-                        row0.put(columnLabels.get(j), rs.getObject(columnLabels.get(j)));
-                    }
+            while (rs.next()) {
+                Map<String, Object> row = new HashMap();
+                for (int j = 0; j < columnLabels.size(); j++) {
+                    row.put(columnLabels.get(j), rs.getObject(columnLabels.get(j)));
                 }
 
-                if (rs.next()) {
-                    for (int j = 0; j < columnLabels.size(); j++) {
-                        row1.put(columnLabels.get(j), rs.getObject(columnLabels.get(j)));
-                    }
-                }
-
-                if (row0.isEmpty() && row1.isEmpty()) break;
-
-                if (!row0.isEmpty() && row1.isEmpty()) {
-                    process.processOneRow(row0);
-                    callback.callAfterProcess();
-
+                if (rs.isLast()) {
                     process.setLastRow();
-
-                    process.processOneRow(row1);
-                    callback.callAfterProcess();
                 }
 
-                if (row0.isEmpty() && !row1.isEmpty()) {
-                    process.processOneRow(row0);
-                    callback.callAfterProcess();
-
-                    process.processOneRow(row1);
-                    callback.callAfterProcess();
-                }
-
+                process.processOneRow(row);
+                callback.callAfterProcess();
             }
 
         } catch (SQLException e) {
